d41cc6807dfd0e30e4b75775b9ef7808938e45fc
lbrahimali <ibroemail1@gmail.com>
2024-08-30 01:38:16 +0300

    payment

--- src/pages/paymentFailed.jsx
+++ src/pages/paymentFailed.jsx
@@ -1,27 +1,34 @@
-import failed_card from "../assets/img/failed_card.png";
+import { useLocation } from "react-router-dom";
+import NotFound from "./404";
+// import failed_card from "../assets/img/failed_card.png";
 
 const PaymentFailed = () => {
-  return (
-    <section className="min-h-0 flex flex-col justify-start items-center pt-24">
-      <div className="w-[25rem] overflow-visible">
-        <img src={failed_card} alt="failed_card" />
-      </div>
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const unAuthorized = currentPath == "/payment-failed";
+  window.parent.postMessage({ status: "failed" }, "*");
 
-      <div className="pt-8 text-center">
-        <p className="text-3xl mb-4 text-[--black-2]">Ödeme Başarısız !</p>
-        <p className="text-[--gr-1]">
-          İşleminiz teknik bir hata nedeniyle başarısız oldu. Lütfen tekrar
-          deneyin.
-        </p>
-      </div>
-
-      <div className="w-full flex justify-center pt-14">
-        <button className="flex items-center py-2.5 whitespace-nowrap px-4 rounded-md text-sm border-[1.5px] disabled:cursor-not-allowed justify-center text-[--white-1] bg-[--primary-1] border-[--primary-1] group border-none">
-          Geri dön
-        </button>
-      </div>
-    </section>
-  );
+  return unAuthorized && <NotFound />;
 };
 
 export default PaymentFailed;
+
+// <section className="min-h-0 flex flex-col justify-start items-center pt-24">
+//   <div className="w-[25rem] overflow-visible">
+//     <img src={failed_card} alt="failed_card" />
+//   </div>
+
+//   <div className="pt-8 text-center">
+//     <p className="text-3xl mb-4 text-[--black-2]">Ödeme Başarısız !</p>
+//     <p className="text-[--gr-1]">
+//       İşleminiz teknik bir hata nedeniyle başarısız oldu. Lütfen tekrar
+//       deneyin.
+//     </p>
+//   </div>
+
+//   <div className="w-full flex justify-center pt-14">
+//     <button className="flex items-center py-2.5 whitespace-nowrap px-4 rounded-md text-sm border-[1.5px] disabled:cursor-not-allowed justify-center text-[--white-1] bg-[--primary-1] border-[--primary-1] group border-none">
+//       Geri dön
+//     </button>
+//   </div>
+// </section>
--- src/pages/paymentSuccess.jsx
+++ src/pages/paymentSuccess.jsx
@@ -1,42 +1,13 @@
-// import Lottie from "lottie-react";
-// import checkAnim from "../assets/anim/lottie/check_anim.json";
-// import congraAnim from "../assets/anim/lottie/congra_anim.json";
-// import { useEffect, useState } from "react";
-// import { usePopup } from "../context/PopupContext";
-// import TypingEffect from "../components/common/typingEffect";
+import { useLocation } from "react-router-dom";
+import NotFound from "./404";
 
 const PaymentSuccess = () => {
-  // Notify the parent window that the success page has loaded
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const unAuthorized = currentPath == "/payment-success";
   window.parent.postMessage({ status: "success" }, "*");
 
-  return null;
+  return unAuthorized && <NotFound />;
 };
 
 export default PaymentSuccess;
-
-// <section
-//   id="payment_success-123"
-//   className="min-h-0 flex justify-center items-start"
-// >
-//   <div className="w-[325px] overflow-visible relative">
-//     <div className="w-full h-full bg-[--white-1] flex justify-center items-center relative overflow-visible">
-//       <div className="absolute w-full h-full top-0 left-0">
-//         {playAnimation && (
-//           <Lottie animationData={congraAnim} loop={false} />
-//         )}
-//       </div>
-//       <div className="w-60">
-//         <Lottie animationData={checkAnim} loop={false} />
-//       </div>
-//     </div>
-//     <div className="flex w-full justify-center">
-//       <div className="text-[--green-1] text-xl mt-10 w-60">
-//         <TypingEffect
-//           text="Ödeme işleminiz başarlı."
-//           speed={40}
-//           delay={4000}
-//         />
-//       </div>
-//     </div>
-//   </div>
-// </section>

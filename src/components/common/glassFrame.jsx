015c5e01e822534b0b2db9cbfd6e9a2aa0b46753
lbrahimali <ibroemail1@gmail.com>
2024-09-11 12:11:15 +0300

    login video

--- src/components/common/glassFrame.jsx
+++ src/components/common/glassFrame.jsx
@@ -1,20 +1,32 @@
 import imgUrl from "../../assets/img/pentegrasyon.png";
+import VideoLoop from "./videoLoop";
 
 const GlassFrame = ({ component, className }) => {
   return (
     <section
-      className={`px-[4%] pt-36 bg-no-repeat relative ${className}`}
+      className={`px-[4%] pt-36 bg-no-repeat relative bg-black ${className}`}
       style={{
         backgroundImage: `url(${imgUrl})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div className="absolute top-0 left-0  bg-gradient-to-r from-[--primary-1] to-indigo-100 inline-block text-transparent bg-clip-text text-2xl p-4 font-[conthrax]">
+      <div className="absolute top-0 left-0 right-0  bg-gradient-to-r from-[--primary-1] to-indigo- inline-block text-transparent bg-clip-text text-2xl p-4 font-[conthrax] text-center text-white z-[999]">
         Pentegrasyon
       </div>
+      <VideoLoop />
       <div className="max-w-md mx-auto p-6 pt-10 text-[--white-1] bg-gray-400 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border border-[--gr-1]">
         {component}
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 z-[999] text-white text-center font-light py-3 px-[4%] text-sm bg-indigo-500/60">
+        Pentegrasyon bir LiwaSoft iştirakidir.{" "}
+        <span className="max-sm:hidden">- </span>
+        <span className="max-sm:block">
+          Müşteri Hizmetleri :{" "}
+          <a href="tel:08508407807" className="text-lime-400">
+            0850 840 78 07
+          </a>
+        </span>
       </div>
     </section>
   );
---
+++ src/components/common/videoLoop.jsx
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+import videoSRC from "../../assets/video/pentevideo.mp4";
+
+const VideoLoop = () => {
+  const videoRef = useRef(null);
+
+  return (
+    <div className="w-full h-full absolute top-0 bottom-0 left-0 right-0">
+      <div className="absolute bg-black/70 inset-0"></div>
+      <video
+        ref={videoRef}
+        className="object-cover h-full"
+        width="100%"
+        height="100%"
+        loop
+        autoPlay
+        muted
+        // controls
+      >
+        <source src={videoSRC} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      {/* <div class="absolute inset-0 bg-gradient-to-t from-indigo-600 via-indigo-500 to-transparent opacity-70 h-16"></div> */}
+    </div>
+  );
+};
+
+export default VideoLoop;

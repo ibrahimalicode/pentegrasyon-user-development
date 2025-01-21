fc5a106443dcf14edd3b787f617eee339dfa95a9
lbrahimali <ibroemail1@gmail.com>
2025-01-21 18:21:22 +0300

    mogros restaurant

--- src/components/orders/yemekSepeti/yemekSepetiOrderDetails.jsx
+++ src/components/orders/yemekSepeti/yemekSepetiOrderDetails.jsx
@@ -181,10 +181,10 @@ const YemekSepetiOrderDetails = ({ order, setOrdersData }) => {
         )}
 
         <table className="rounded-md overflow-clip h-max">
-          <thead className="bg-[--light-3]">
+          <thead className="bg-[--light-3]-">
             <tr>
-              <th className="p-2 font-normal text-left">Ürün</th>
-              <th className="p-2 font-normal text-right">Tutar</th>
+              <th className="p-2- font-normal text-left"></th>
+              <th className="p-2- font-normal text-right"></th>
             </tr>
           </thead>
 
--- src/enums/restaurantStatuses.js
+++ src/enums/restaurantStatuses.js
@@ -3,7 +3,10 @@ const RestaurantStatuses = [
     { label: "Açık", value: true, id: 100, key: "status" },
     { label: "Kapalı", value: false, id: 300 },
   ],
-  [],
+  [
+    { label: "Açık", value: true, id: true, key: "active" },
+    { label: "Kapalı", value: false, id: true },
+  ],
   [],
   [
     { label: "Açık", value: true, id: "open", key: "availabilityState" },

5d4a27ac29fa574d00499101838a33a820190a30
lbrahimali <ibroemail1@gmail.com>
2025-03-12 02:20:39 +0300

    UI Fix

--- src/components/common/customToggle.jsx
+++ src/components/common/customToggle.jsx
@@ -23,7 +23,7 @@ const CustomToggle = ({
         className="sr-only peer"
       />
       <div
-        className={`relative w-[52.5px] h-[28px] bg-[--light-4] rounded-full peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-[--white-1] after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-[--white-1] after:border-[--light-4] after:border after:rounded-full after:h-6 after:w-6 transition-colors after:transition-transform duration-500 after:duration-500 ease-in-out after:ease-in-out peer-checked:bg-[--primary-1] ${className}`}
+        className={`relative w-[52.5px] h-[28px] bg-[--gr-1] rounded-full peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-[--white-1] after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-[--white-1] after:border-[--gr-1] after:border after:rounded-full after:h-6 after:w-6 transition-colors after:transition-transform duration-500 after:duration-500 ease-in-out after:ease-in-out peer-checked:bg-[--primary-1] ${className}`}
       ></div>
 
       <span className={`ml-4 text-sm text-[--gr-1] ${className2}`}>

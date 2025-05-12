f96a72b734fb2f51cd93919c834a7e5b15ea7dc4
lbrahimali <ibroemail1@gmail.com>
2025-05-12 11:14:06 +0300

    Custom Tost Dark/Light theme UI Fix

--- src/components/common/customToast.jsx
+++ src/components/common/customToast.jsx
@@ -34,13 +34,13 @@ export default function CustomToast({ color, message, t }) {
           <div className="flex-shrink-0 pt-0.5"></div>
           <div className="flex-1">
             <p
-              className="text-sm font-medium text-gray-900"
+              className="text-sm font-medium text-[--black-2]"
               dangerouslySetInnerHTML={{ __html: message.title }}
             >
               {/* {message.title} */}
             </p>
             <p
-              className="mt-1 text-sm text-gray-500"
+              className="mt-1 text-sm text-[--gr-1]"
               dangerouslySetInnerHTML={{ __html: message.content }}
             >
               {/* {message.content} */}
@@ -48,7 +48,7 @@ export default function CustomToast({ color, message, t }) {
           </div>
         </div>
       </div>
-      <div className="flex border-l border-gray-200">
+      <div className="flex border-l border-[--gr-3]">
         <button
           onClick={() => toast.dismiss(t?.id)}
           className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-[--red-1] hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"

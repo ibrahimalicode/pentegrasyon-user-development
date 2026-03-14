20476c332e63479b2149e1e32383e4c92421bf1c
lbrahimali <ibroemail1@gmail.com>
2026-03-14 12:54:22 +0300

    Yemek Çeki label fix

--- src/enums/trendyolPaymentMethods.js
+++ src/enums/trendyolPaymentMethods.js
@@ -1,5 +1,5 @@
 export const PaymentMethods = [
   { value: "PAY_WITH_CARD", label: "Online Kredi Kartı" },
   { value: "PAY_WITH_ON_DELIVERY", label: "Kapıda Ödeme" },
-  { value: "PAY_WITH_MEAL_CARD", label: "Yemek Kartı ile Ödeme" },
+  { value: "PAY_WITH_MEAL_CARD", label: "Yemek Çeki" },
 ];
